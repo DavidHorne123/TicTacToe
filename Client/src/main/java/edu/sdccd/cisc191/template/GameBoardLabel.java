bfc8a43dd177416aabc40acd151d6a25821adfc1
DavidHorne123 <dhorne6000@gmail.com>
2024-11-27 16:38:41 -0800

    Modified the gameboardlabel class

--- Client/src/main/java/edu/sdccd/cisc191/template/GameBoardLabel.java
+++ Client/src/main/java/edu/sdccd/cisc191/template/GameBoardLabel.java
@@ -6,41 +6,19 @@ import javafx.scene.control.Label;
 /**
  * Extends the basic JavaFX Label with game functionality
  */
-public class GameBoardLabel<T> extends Label {
+public class GameBoardLabel extends Label {
 
     public static Insets LABEL_PADDING = new Insets(20);
 
     // Generic data associated with this label
-    private T associatedData;
+
 
     public GameBoardLabel() {
         // set Label properties like padding
         // adds space between the text
         setPadding(LABEL_PADDING);
     }
-    /**
-     * Gets the data associated with this label
-     * @return the associated data
-     */
-    public T getAssociatedData() {
-        return associatedData;
-    }
-    /**
-     * Sets the data associated with this label
-     * @param associatedData the data to associate with this label
-     */
-    public void setAssociatedData(T associatedData) {
-        this.associatedData = associatedData;
-    }
 
-    /**
-     * Updates the label's text to match the associated data (if not null)
-     */
-    public void updateLabelText() {
-        if (associatedData != null) {
-            setText(associatedData.toString());
-        }
-    }
 
 
 }
